29c726541a69e0cd150755cec24ba776a86205a5
erasmus <erasmuschawey12345@gmail.com>
2025-03-13 11:46:28 +0000

    made changes to the nav icon

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,26 +1,29 @@
 <template>
-  <div class="min-h-screen bg-gray-100 flex flex-col md:flex-row">
+  <div class="min-h-screen bg-gray-100 flex flex-col md:flex-row ">
     <!-- Hamburger Menu Button (Mobile Only) -->
-    <button 
-      @click="isSidebarOpen = !isSidebarOpen"
-      class="md:hidden fixed top-4 left-4 z-50 p-2 bg-blue-900 text-white rounded-lg focus:outline-none"
-    >
-      <!-- Modern Hamburger Icon (SVG) -->
-      <svg 
-        class="w-6 h-6" 
-        fill="none" 
-        stroke="currentColor" 
-        viewBox="0 0 24 24" 
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path 
-          stroke-linecap="round" 
-          stroke-linejoin="round" 
-          stroke-width="2" 
-          d="M4 6h16M4 12h16M4 18h16"
-        />
-      </svg>
-    </button>
+
+<!-- Hamburger Menu Button (Mobile Only) -->
+<button 
+  @click="isSidebarOpen = !isSidebarOpen"
+  class="md:hidden fixed top-4 right-4 z-50 p-2 bg-blue-900 text-white rounded-lg focus:outline-none"
+>
+  <!-- Modern Hamburger Icon (SVG) -->
+  <svg 
+    class="w-6 h-6" 
+    fill="none" 
+    stroke="currentColor" 
+    viewBox="0 0 24 24" 
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path 
+      stroke-linecap="round" 
+      stroke-linejoin="round" 
+      stroke-width="2" 
+      d="M4 6h16M4 12h16M4 18h16"
+    />
+  </svg>
+</button>
+
 
     <!-- Sidebar -->
     <div 
